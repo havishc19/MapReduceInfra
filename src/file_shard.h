5ebcea16d3617283d094ae3a527a1d32d0db1bbb
abansal88 <abansal88@gatech.edu>
2018-12-07 23:30:16 -0500

    fileshard and mr_spec code running

--- src/file_shard.h
+++ src/file_shard.h
@@ -2,16 +2,53 @@
 
 #include <vector>
 #include "mapreduce_spec.h"
+#include <cstdio>
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 
 /* CS6210_TASK: Create your own data structure here, where you can hold information about file splits,
      that your master would use for its own bookkeeping and to convey the tasks to the workers for mapping */
 struct FileShard {
-
+	int id;
+	string filename;
+	int startByte;
+	int endByte;
 };
 
 
 /* CS6210_TASK: Create fileshards from the list of input files, map_kilobytes etc. using mr_spec you populated  */ 
 inline bool shard_files(const MapReduceSpec& mr_spec, std::vector<FileShard>& fileShards) {
+	int shardSize = mr_spec.mapKilobytes;
+	vector<string> inputFiles = mr_spec.inFiles();
+	// int shardSize = 500;
+	// vector<string> inputFiles{"../test/input/testdata_1.txt"};
+	int counter = 0;
+	for(int i=0; i<inputFiles.size(); i++) {
+		ifstream fileObj(inputFiles[i]);
+		string line;
+		// fileObj.open(inputFiles[i]);
+		int start = 0;
+		int end = 0;
+		int curBytes = 0;
+		if(fileObj.is_open()) {
+			while(getline(fileObj, line)) {
+				if(curBytes + line.length() <= shardSize) {
+					curBytes += line.length();
+				} else {
+					struct FileShard fs;
+					fs.id = counter++; 
+					fs.filename = inputFiles[i];
+					fs.startByte = start;
+					fs.endByte = start+curBytes;
+					fileShards.push_back(fs);
+					start += curBytes+1;
+					curBytes = line.length();
+				}
+			}
+			fileObj.close();
+		}
+	}
 	return true;
 }
--- src/mapreduce_spec.h
+++ src/mapreduce_spec.h
@@ -1,16 +1,69 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
 /* CS6210_TASK: Create your data structure here for storing spec from the config file */
 struct MapReduceSpec {
-
+	int numWorkers;
+	vector<string> workerAddr;
+	vector<string> inFiles;
+	string outDir;
+	int numOutFiles;
+	int mapKilobytes;
+	string userID;
 };
 
+void parseCSV(string input, vector<string>& retVal) {
+	string temp = "";
+	for(int i=0; i<input.length(); i++) {
+		if(input[i] == ',') {
+			retVal.push_back(temp);
+			temp = "";
+		} else {
+			temp += string(1, input[i]);
+		}
+	}
+	retVal.push_back(temp);
+	return;
+}
 
 /* CS6210_TASK: Populate MapReduceSpec data structure with the specification from the config file */
 inline bool read_mr_spec_from_config_file(const std::string& config_filename, MapReduceSpec& mr_spec) {
+	ifstream inFile(config_filename);
+	string line;
+	if(inFile.is_open()) {
+		while(getline(inFile, line)) {
+			string key = line.substr(0, line.find("="));
+			string val = line.substr(line.find("=")+1, line.length());
+			if(key == "n_workers") {
+				mr_spec.numWorkers = stoi(val);
+			}
+			if(key == "worker_ipaddr_ports") {
+				parseCSV(val, mr_spec.workerAddr);
+			}
+			if(key == "input_files") {
+				parseCSV(val, mr_spec.inFiles);
+			}
+			if(key == "output_dir") {
+				mr_spec.outDir = val;
+			}
+			if(key == "n_output_files") {
+				mr_spec.numOutFiles = stoi(val);
+			}
+			if(key == "map_kilobytes") {
+				mr_spec.mapKilobytes = stoi(val);
+			}
+			if(key == "user_id") {
+				mr_spec.userID = val;
+			}
+		}
+		inFile.close();
+	}	
 	return true;
 }
 
@@ -18,4 +71,20 @@ inline bool read_mr_spec_from_config_file(const std::string& config_filename, Ma
 /* CS6210_TASK: validate the specification read from the config file */
 inline bool validate_mr_spec(const MapReduceSpec& mr_spec) {
 	return true;
+}
+
+void printSpec(struct MapReduceSpec& mr_spec) {
+	cout<<mr_spec.numWorkers<<endl;
+	cout<<mr_spec.outDir<<endl;
+	cout<<mr_spec.numOutFiles<<endl;
+	cout<<mr_spec.mapKilobytes<<endl;
+	cout<<mr_spec.userID<<endl;
+	for(int i=0; i<mr_spec.workerAddr.size(); i++) {
+		cout<<mr_spec.workerAddr[i]<<":";
+	}
+	cout<<endl;
+	for(int i=0; i<mr_spec.inFiles.size(); i++) {
+		cout<<mr_spec.inFiles[i]<<":";
+	}
+	cout<<endl;
 }
